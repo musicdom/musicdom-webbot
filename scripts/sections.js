c68b7a4b89476af316451f1dbcfa0dd051a05397
musicdom <sasha12071998214@gmail.com>
2025-11-11 18:18:38 +1000

    Create sections.js

---
+++ scripts/sections.js
@@ -0,0 +1,23 @@
+// Константы и данные
+const workerUrl = "https://musicdom-payment.vercel.app/";
+
+const songs = [
+    {
+        code: "456",
+        title: "Песня Музыкальный Дом",
+        description: "Эксклюзивная композиция от Музыкального Дома",
+        image: "logo.jpg",
+        demo: "song/song456_demo.mp3",
+        full: "song/song456_full.mp3",
+        price: 10
+    },
+    {
+        code: "123",
+        title: "Песня для Максима",
+        description: "Индивидуальная песня, созданная специально для Максима",
+        image: "🎶",
+        demo: "songs/song123_demo.mp3",
+        full: "songs/song123_full.mp3",
+        price: 200
+    }
+];
